93f6e937f812d89731f902984c412cbe44d6cab0
Carlos Molina <molina.carlos.dev@gmail.com>
2018-06-15 21:20:27 -0500

    Added Link to landing buttons plus Login Form

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,19 @@ import './App.css';
 import Navbar from './components/layout/Navbar.js';
 import Footer from './components/layout/Footer.js';
 import Landing from './components/layout/Landing.js';
+import Register from './components/auth/Register.js';
+import Login from './components/auth/Login.js';
 class App extends Component {
 	render() {
 		return (
 			<Router>
 				<div className="App">
 					<Navbar />
-					<Route exact path="" component={Landing} />
-					<h1>MY react app</h1>
+					<Route exact path="/" component={Landing} />
+					<div className="container">
+						<Route exact path="/register" component={Register} />
+						<Route exact path="/login" component={Login} />
+					</div>
 					<Footer />
 				</div>
 			</Router>
