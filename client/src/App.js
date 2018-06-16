6598e9120f169e78ff3f518d021f8b804cf8af5a
Carlos Molina <molina.carlos.dev@gmail.com>
2018-06-15 22:25:23 -0500

    Redux initialState and Store plus dev_tools - DONE

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar.js';
@@ -6,20 +8,23 @@ import Footer from './components/layout/Footer.js';
 import Landing from './components/layout/Landing.js';
 import Register from './components/auth/Register.js';
 import Login from './components/auth/Login.js';
+
 class App extends Component {
 	render() {
 		return (
-			<Router>
-				<div className="App">
-					<Navbar />
-					<Route exact path="/" component={Landing} />
-					<div className="container">
-						<Route exact path="/register" component={Register} />
-						<Route exact path="/login" component={Login} />
+			<Provider store={store}>
+				<Router>
+					<div className="App">
+						<Navbar />
+						<Route exact path="/" component={Landing} />
+						<div className="container">
+							<Route exact path="/register" component={Register} />
+							<Route exact path="/login" component={Login} />
+						</div>
+						<Footer />
 					</div>
-					<Footer />
-				</div>
-			</Router>
+				</Router>
+			</Provider>
 		);
 	}
 }
